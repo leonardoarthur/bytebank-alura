8b15346aaf7e654627990fcf81869228fadfa375
Leonardo Arthur <lraguiar.la@gmail.com>
2022-12-10 21:54:01 -0300

    If/when e For Loop/While

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,19 +1,57 @@
-fun main(){
+fun main() {
     println("Bem vindo ao Bytebank")
-    val titular: String = "Leo"
-    val numeroConta: Int = 1000
-    var saldo = 0.0 //Double
-    saldo = 100 + 2.0 //A soma de um Int com Double resulta em Double, sem mudança no tipo da var.
-    saldo += 200
 
-    println("titular $titular")
-    println("número da conta: $numeroConta")
-    println("saldo da conta: $saldo")
+//    for (i in 1..6){
+//        val titular: String = "Leo + $i"
+//        val numeroConta: Int = 1000 + i
+//        var saldo = 0.0 + i//Double
+//
+//        println("titular $titular")
+//        println("número da conta: $numeroConta")
+//        println("saldo da conta: $saldo")
+//    }
+
+//    for (i in 5 downTo 0){
+//        val titular: String = "Leo + $i"
+//        val numeroConta: Int = 1000 + i
+//        var saldo = 0.0 + i//Double
+//
+//        println("titular $titular")
+//        println("número da conta: $numeroConta")
+//        println("saldo da conta: $saldo")
+//    }
+//        for (i in 1..6 step 2){
+//        val titular: String = "Leo + $i"
+//        val numeroConta: Int = 1000 + i
+//        var saldo = 0.0 + i//Double
+//
+//        println("titular $titular")
+//        println("número da conta: $numeroConta")
+//        println("saldo da conta: $saldo")
+//    }
+
+//    val titular: String = "Leo"
+//    val numeroConta: Int = 1000
+//    var saldo = 0.0 //Double
+    var i = 0
+    while (i < 5){ //Enquanto i for menor que 5 execute esse comando:
+        val titular: String = "Leo + $i"
+        val numeroConta: Int = 1000 + i
+        var saldo = 0.0 + i//Double
+
+        println("titular $titular")
+        println("número da conta: $numeroConta")
+        println("saldo da conta: $saldo")
+        i++
+    }
+
+
 
 
 }
-fun testaCondicoes(saldo: Double){
-    if(saldo < 0) {
+
+fun testaCondicoes(saldo: Double) {
+    if (saldo < 0) {
         println("Você está devendo.")
     } else if (saldo == 0.0) {
         println("Saldo zerado!")
@@ -25,9 +63,11 @@ fun testaCondicoes(saldo: Double){
         saldo < 0 -> {
             println("Você está devendo, veja nossas opções de emprestimo para sair dessa situação")
         }
+
         saldo == 0.0 -> {
             println("Saldo zerado! não pode comprar mais nada por hoje.")
         }
+
         else -> {
             println("Seu saldo é positivo, você precisa gastar. Conheça nosso cartão de crédito.")
         }
