2e055ecec2691842c0eac3bcca71b5e22d3ec02d
Leonardo Arthur <lraguiar.la@gmail.com>
2023-05-03 18:14:37 -0300

    Criando nova classe.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,7 +3,9 @@ fun main() {
 
     testaCopiasEReferencias()
 
+    val leo = Funcionario("Leo", "2222222222", 100.0)
+
+    println(leo.cpf)
+    println(leo.salario)
+    println("A bonificação que leo vai receber é de ${leo.bonificacao()}" )
 }
-
-
-
